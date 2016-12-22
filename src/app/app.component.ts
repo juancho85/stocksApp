17e9ff354783a1068d54a1e8e786002ed46ae84d
Juan David Sánchez <juancho85@gmail.com>
2016-12-22 12:55:13 +0100

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StocksDbService} from "./stocks-db.service";
+import {Response} from "@angular/http";
+import {Auth} from './auth.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StocksDbService, Auth]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  httpOutput;
+  items: any[] = [];
+  constructor(private stocksDb : StocksDbService, private auth: Auth){}
+
+  ngOnInit(){
+    this.stocksDb.getData().subscribe(
+      response => this.httpOutput = response,
+      error => console.error(error)
+    );
+  }
+
+  onSubmit(username: string, email : string){
+
+    this.stocksDb.sendData({username: username, email: email}).subscribe(
+      data => console.log(data)
+    );
+  }
+
+  onGetData(){
+    this.stocksDb.getOwnData().subscribe(
+      data => {
+        const myArray = [];
+        for(let key in data){
+          myArray.push(data[key]);
+        }
+        this.items = myArray;
+      },
+      error => console.error(error)
+    );
+  }
+
 }
---
+++ src/app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'sRVJbX1NBF8XRHHRHnwAFCsq0IBFL1Jj',
+    domain: 'juancho.eu.auth0.com'
+};
